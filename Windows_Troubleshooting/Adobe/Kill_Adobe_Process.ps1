faad6a0a1e43e58afcf480ed793453144210104e
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2025-11-07 21:46:23 -0600

    Add files via upload

---
+++ Windows_Troubleshooting/Adobe/Kill_Adobe_Process.ps1
@@ -0,0 +1,19 @@
+﻿function Kill-Adobe-Process {
+
+      $adobe = (Get-Process -Name AcroRd32).ProcessName
+
+      if($adobe.Length -gt 0){  
+        $adobeID = (Get-Process -Name AcroRd32).Id
+
+        forEach($i in $adobeID){
+            Try {
+                Stop-Process -Id $i -Force
+                Write-Host "Process ID $i has been terminated" -BackgroundColor Green -ForegroundColor White
+            } Catch{
+                Write-Host "Failed to terminate Process ID $i" -BackgroundColor Red -ForegroundColor Black
+            }
+        }
+    } else {
+        Write-Host "No Adobe Processes Running"               
+    } 
+} 
---
+++ Windows_Troubleshooting/Adobe/Update_Adobe.ps1
@@ -0,0 +1,31 @@
+﻿function Update-Adobe {
+
+    if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+            Write-Host 'Not running as admin'
+            return
+    } else {
+        Write-Host 'Running as admin'
+    }
+
+    $adobeUpdateUrl = "https://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/2300920053/AcroRdrDC2300920053_en_US.exe"
+
+    $installerPath = "$env:TEMP\AcroRdrDC_Update.exe"
+
+    Invoke-WebRequest -Uri $adobeUpdateUrl -OutFile $installerPath
+
+    Try {
+        Start-Process -FilePath $installerPath -ArgumentList "/sAll /rs /msi EULA_ACCEPT=YES" -Wait   
+        Write-Host "Adobe successfully updated"  -BackgroundColor Green -ForegroundColor White
+    } Catch {
+        Write-Host "Adobe failed to install" -BackgroundColor Red -ForegroundColor Black
+    }
+
+    Try {
+        Remove-Item -Path $installerPath -Force
+        Write-Host: "Installer successfully removed"  -BackgroundColor Green -ForegroundColor White
+    } Catch {
+        Write-Host "Installer failed to be removed" -BackgroundColor Red -ForegroundColor Black
+    }
+}
+
+ 

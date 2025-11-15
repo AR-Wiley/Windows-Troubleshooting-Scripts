838a9758fc1f4ca21d845607561de3c6d5fc4d22
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2025-11-14 21:10:14 -0600

    Add files via upload

---
+++ Windows_Troubleshooting/Adobe/RepairAdobe.ps1
@@ -0,0 +1,26 @@
+﻿function RepairAdobe {
+
+    $AdobePath = "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader"
+    $ExePath = Join-Path $AdobePath "AcroRd32.exe"
+
+    if(-not(Test-Path $AdobePath)) {
+        Write-Host "Path for Adobe does not exist"
+        return
+    }
+
+    if(-not(Test-Path $ExePath)) {
+        Write-Host "Executable for Adobe does not exist"
+        return
+    } else {
+        Try{	
+	        Write-Host "Starting Adobe Repair"
+            & $ExePath "/repair"
+            Write-Host "Adobe Repair succesfully completed"
+        } catch {
+            Write-Host "ERROR MESSAGE:" -ForegroundColor Red
+            Write-Host $_.Exception.Message 
+        }
+    }
+}
+
+RepairAdobe

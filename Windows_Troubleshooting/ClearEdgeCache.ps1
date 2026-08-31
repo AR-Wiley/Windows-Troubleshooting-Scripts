3295abaa7ed7d63e803022f0c19c9e08f292e99c
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2026-08-31 17:09:59 -0500

    Add files via upload

---
+++ Windows_Troubleshooting/ClearEdgeCache.ps1
@@ -0,0 +1,31 @@
+﻿
+$adminLog = "$env:USERPROFILE\Admin_Logs"
+
+
+$edgeApplicaiton = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+
+$edgeCachePath = "$env:USERPROFILE\AppData\Local\Microsoft\Edge\User Data\Default\Cache\Cache_Data\" 
+
+
+function Validate-Admin-Log{
+
+    if(-not(Test-Path $adminLog)){
+        try
+        {
+            New-Item -ItemType Directory -Path $adminLog
+                
+        }
+        catch
+        {
+            Write-Output "An error has occured: $_"
+        }
+    
+    }
+}
+
+function Validate-Edge {
+    
+
+
+}
+

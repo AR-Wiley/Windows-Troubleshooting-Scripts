2097913fab282d7841ef1d002456705f1c4583d2
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2026-09-09 20:31:50 -0500

    Refactor Edge cache clearing script

--- Windows_Troubleshooting/ClearEdgeCache.ps1
+++ Windows_Troubleshooting/ClearEdgeCache.ps1
@@ -1,18 +1,35 @@
 ﻿
+$edgeApplication = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+$edgeCachePath  = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Cache\Cache_Data"
+$edgeProcess = (Get-Process -Name msedge).ProcessName
 $adminLog = "$env:USERPROFILE\Admin_Logs"
+$cacheRemovedLog = Join-Path $adminLog "cleared_cache.log" 
+$date = Get-Date
 
 
-$edgeApplicaiton = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+function Validate-Edge-Application {
 
-$edgeCachePath = "$env:USERPROFILE\AppData\Local\Microsoft\Edge\User Data\Default\Cache\Cache_Data\" 
+    if(!(Test-Path -Path $edgeApplication)){
+        Write-Host "Edge Application path does not exist"
+        return
+    } 
+}
 
+function Validate-Cache-Path {
 
-function Validate-Admin-Log{
+    if(!(Test-Path -Path $edgeCachePath)){
+        Write-Host "Edge Cache path does not exist"
+        return
+    } 
 
+}
+
+function Validate-Log-Path {
     if(-not(Test-Path $adminLog)){
         try
         {
             New-Item -ItemType Directory -Path $adminLog
+            Write-Host "Admin Log Folder has been created"
                 
         }
         catch
@@ -23,9 +40,52 @@ function Validate-Admin-Log{
     }
 }
 
-function Validate-Edge {
-    
 
+function Kill-Edge-Process {
+
+    if(Get-Process -Name $edgeProcess -ErrorAction SilentlyContinue){
+        try
+        {
+            Stop-Process -Name $edgeProcess -Force -ErrorAction Stop
+            Write-Host "Edge Browser has been terminated" 
+        }
+        catch
+        {
+            Write-Host "An error has occured $_"
+        }
+    }
 
 }
 
+function Clear-Edge-Cache {
+        
+        try
+        {
+            Get-ChildItem -Path $edgeCachePath -Recurse | Remove-Item -Force -Recurse -ErrorAction Stop
+            Write-Host "Edge cache has been cleared"
+            "$date - Edge cache has been cleared" | Out-File -FilePath $cacheRemovedLog -Append
+                
+        }
+        catch
+        {
+            Write-Host "An error has occured $_"
+            "$date - An error has occured $_" | Out-File -FilePath $cacheRemovedLog -Append
+        }
+        
+        
+ }
+    
+
+Validate-Edge-Application
+Validate-Cache-Path
+Validate-Log-Path
+Kill-Edge-Process
+Clear-Edge-Cache
+
+
+    
+#### Notes ####
+    
+# Recurse -> looks inside every single subfolder for files
+# Force -> it allows you to delete hidden files, system files, and read-only files without PowerShell stopping to ask you for permission
+        
